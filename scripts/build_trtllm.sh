0e434af90e868b16d11d12418aa8eab284343942
htringuyen <tringuyenh75@gmail.com>
2025-11-29 13:02:16 +0000

    docker: use new clion version

--- scripts/build_trtllm.sh
+++ scripts/build_trtllm.sh
@@ -10,4 +10,3 @@ python3 ./scripts/build_wheel.py \
     --clean \
     --cuda_architectures "86-real" \
     --job_count 10 \
-    --extra-cmake-vars="-DCMAKE_VERBOSE_MAKEFILE=ON" 2>&1 | tee build.log
